93bcb9616cdaa1f87d657d46e8bc60ebf211e8ca
AyoubHolmes <boulbaz147@gmail.com>
2021-10-08 19:58:19 +0100

    test

--- module_02/ex02/Fixed.cpp
+++ module_02/ex02/Fixed.cpp
@@ -55,6 +55,36 @@ bool	Fixed::operator != (Fixed &F) {
 	return (fixed_point_value != F.fixed_point_value);
 }
 
+Fixed    Fixed::operator + (Fixed &F2)
+{
+	Fixed F3; 
+	F3.fixed_point_value = this->fixed_point_value + F2.getRawBits();
+	// return (Fixed(this->fixed_point_value + F2.getRawBits()));
+	return (F3);
+}
+
+Fixed    Fixed::operator * (Fixed &F2)
+{
+	Fixed F3;
+	F3.fixed_point_value = (this->fixed_point_value * F2.getRawBits());
+	F3.fixed_point_value = F3.toInt();
+	return (F3);
+}
+
+Fixed    Fixed::operator - (Fixed &F2)
+{
+	Fixed F3;
+	F3.fixed_point_value = (this->fixed_point_value - F2.getRawBits());
+	return (F3);
+}
+
+
+Fixed    Fixed::operator / (Fixed &F2)
+{
+	Fixed F3 (this->fixed_point_value / F2.getRawBits());
+	return (F3);
+}
+
 int		Fixed::getRawBits( void ) const{
 	return (fixed_point_value);
 }
@@ -62,6 +92,77 @@ int		Fixed::getRawBits( void ) const{
 void	Fixed::setRawBits( int const raw ){
 	fixed_point_value = raw;
 }
+
+float	Fixed::toFloat( void ) const{
+	return ((float)fixed_point_value / (1 << Fixed::fractional_bits));
+}
+
+int		Fixed::toInt( void ) const{
+	return ((int)fixed_point_value / (1 << Fixed::fractional_bits));
+}
+
+Fixed     Fixed::operator ++ ()
+{
+	Fixed tmp;
+
+	tmp.fixed_point_value = fixed_point_value;
+	++fixed_point_value;
+	return (tmp);
+}
+
+Fixed     Fixed::operator ++ (int)
+{
+	++fixed_point_value;
+	return (*this);
+}
+
+Fixed     Fixed::operator -- ()
+{
+	Fixed tmp;
+
+	tmp.fixed_point_value = fixed_point_value;
+	--fixed_point_value;
+	return (tmp);
+}
+
+Fixed     Fixed::operator -- (int)
+{
+	--fixed_point_value;
+	return (*this);
+}
+
+std::ostream &operator<<(std::ostream& os, Fixed const & F) {
+	os << F.toFloat();
+	return (os);
+}
+Fixed    &Fixed::min(Fixed &F1, Fixed &F2)
+{
+	if (F1 >= F2)
+		return (F2);
+	return (F1);
+}
+
+const Fixed    &Fixed::min(const Fixed &F1, const Fixed &F2)
+{
+	if ((Fixed&)F1 >= (Fixed&)F2)
+		return (F2);
+	return (F1);
+}
+
+Fixed    &Fixed::max(Fixed &F1, Fixed &F2)
+{
+	if (F1 <= F2)
+		return (F2);
+	return (F1);
+}
+
+const Fixed    &Fixed::max(const Fixed &F1, const Fixed &F2)
+{
+	if ((Fixed&)F1 <= (Fixed&)F2)
+		return (F2);
+	return (F1);
+}
+
 
 Fixed::~Fixed()
 {
