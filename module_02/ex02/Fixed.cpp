1ca8dcb7cb65790bd8793fd7eb905a09c4eb0832
AyoubHolmes <boulbaz147@gmail.com>
2021-10-09 11:17:15 +0100

    test

--- module_02/ex02/Fixed.cpp
+++ module_02/ex02/Fixed.cpp
@@ -2,68 +2,67 @@
 
 Fixed::Fixed()
 {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 	fixed_point_value = 0;
 }
 
 Fixed::Fixed(const int val)
 {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 	fixed_point_value = val << Fixed::fractional_bits;
 }
 
 Fixed::Fixed(const float val)
 {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 	fixed_point_value = round(val * (1 << Fixed::fractional_bits));
 }
 
-Fixed::Fixed(Fixed &F)
+Fixed::Fixed(Fixed const &F)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "Copy constructor called" << std::endl;
 	fixed_point_value = F.fixed_point_value;
 }
 
-Fixed     &Fixed::operator = (Fixed &F)
+Fixed     &Fixed::operator = (Fixed const &F)
 {
-	std::cout << "Assignation operator called" << std::endl;
+	// std::cout << "Assignation operator called" << std::endl;
 	fixed_point_value = F.fixed_point_value;
 	return (*this);
 }
 
-bool	Fixed::operator > (Fixed &F) {
+bool	Fixed::operator > (Fixed const &F) {
 	return (fixed_point_value > F.fixed_point_value);
 }
 
-bool	Fixed::operator < (Fixed &F) {
+bool	Fixed::operator < (Fixed const &F) {
 	return (fixed_point_value < F.fixed_point_value);
 }
 
-bool	Fixed::operator >= (Fixed &F) {
+bool	Fixed::operator >= (Fixed const &F) {
 	return (fixed_point_value >= F.fixed_point_value);
 }
 
-bool	Fixed::operator <= (Fixed &F) {
+bool	Fixed::operator <= (Fixed const &F) {
 	return (fixed_point_value <= F.fixed_point_value);
 }
 
-bool	Fixed::operator == (Fixed &F) {
+bool	Fixed::operator == (Fixed const &F) {
 	return (fixed_point_value == F.fixed_point_value);
 }
 
-bool	Fixed::operator != (Fixed &F) {
+bool	Fixed::operator != (Fixed const &F) {
 	return (fixed_point_value != F.fixed_point_value);
 }
 
-Fixed    Fixed::operator + (Fixed &F2)
+Fixed    Fixed::operator + (Fixed const &F2)
 {
 	Fixed F3; 
 	F3.fixed_point_value = this->fixed_point_value + F2.getRawBits();
-	// return (Fixed(this->fixed_point_value + F2.getRawBits()));
 	return (F3);
 }
 
-Fixed    Fixed::operator * (Fixed &F2)
+Fixed    Fixed::operator* (Fixed const &F2)
 {
 	Fixed F3;
 	F3.fixed_point_value = (this->fixed_point_value * F2.getRawBits());
@@ -71,7 +70,7 @@ Fixed    Fixed::operator * (Fixed &F2)
 	return (F3);
 }
 
-Fixed    Fixed::operator - (Fixed &F2)
+Fixed    Fixed::operator - (Fixed const &F2)
 {
 	Fixed F3;
 	F3.fixed_point_value = (this->fixed_point_value - F2.getRawBits());
@@ -79,7 +78,7 @@ Fixed    Fixed::operator - (Fixed &F2)
 }
 
 
-Fixed    Fixed::operator / (Fixed &F2)
+Fixed    Fixed::operator / (Fixed const &F2)
 {
 	Fixed F3 (this->fixed_point_value / F2.getRawBits());
 	return (F3);
@@ -103,6 +102,12 @@ int		Fixed::toInt( void ) const{
 
 Fixed     Fixed::operator ++ ()
 {
+	fixed_point_value++;
+	return (*this);
+}
+
+Fixed     Fixed::operator ++ (int)
+{
 	Fixed tmp;
 
 	tmp.fixed_point_value = fixed_point_value;
@@ -110,13 +115,8 @@ Fixed     Fixed::operator ++ ()
 	return (tmp);
 }
 
-Fixed     Fixed::operator ++ (int)
-{
-	++fixed_point_value;
-	return (*this);
-}
 
-Fixed     Fixed::operator -- ()
+Fixed     Fixed::operator -- (int)
 {
 	Fixed tmp;
 
@@ -125,7 +125,7 @@ Fixed     Fixed::operator -- ()
 	return (tmp);
 }
 
-Fixed     Fixed::operator -- (int)
+Fixed     Fixed::operator -- ()
 {
 	--fixed_point_value;
 	return (*this);
@@ -166,5 +166,5 @@ const Fixed    &Fixed::max(const Fixed &F1, const Fixed &F2)
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
+	// std::cout << "Destructor called" << std::endl;
 }
--- module_02/ex02/main.cpp
+++ module_02/ex02/main.cpp
@@ -1,13 +1,15 @@
-#include <iostream>
 #include "Fixed.hpp"
+#include <iostream>
 
 int main( void ) {
-    Fixed a;
-    Fixed b( a );
-    Fixed c (2);
-    c = b;
-    std::cout << a.getRawBits() << std::endl;
-    std::cout << b.getRawBits() << std::endl;
-    std::cout << c.getRawBits() << std::endl;
-    return 0;
+Fixed a;
+Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+std::cout << a << std::endl;
+std::cout << --a << std::endl;
+std::cout << a << std::endl;
+std::cout << a-- << std::endl;
+std::cout << a << std::endl;
+std::cout << b << std::endl;
+std::cout << Fixed::max( a, b ) << std::endl;
+return 0;
 }
