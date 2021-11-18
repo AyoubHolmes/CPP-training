f7eb0aa34b2d04c8d7560314b165460869f19dc9
AyoubHolmes <boulbaz147@gmail.com>
2021-11-18 15:00:55 +0100

    test

--- module_04/ex01/main.cpp
+++ module_04/ex01/main.cpp
@@ -3,6 +3,9 @@
 
 int main(int argc, char const *argv[])
 {
-    
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    delete j;//should not create a leak
+    delete i;
     return 0;
 }
